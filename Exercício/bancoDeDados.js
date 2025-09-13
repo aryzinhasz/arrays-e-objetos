8cdd4b8e90edbe761e962770b11c60eb7c93402a
Cyndja Aryelle <168868280+aryzinhasz@users.noreply.github.com>
2025-09-12 21:42:54 -0300

    Create bancoDeDados.js

---
+++ Exercício/bancoDeDados.js
@@ -0,0 +1,12 @@
+// Criando um array de livros
+var biblioteca = [
+    {
+        titulo: "O Senhor dos Anéis",
+        autor: "J.R.R. Tolkien",
+        anoPublicacao: 1954,
+    },
+    { titulo: "Dom Casmurro", autor: "Machado de Assis", anoPublicacao: 1899 },
+    { titulo: "1984", autor: "George Orwell", anoPublicacao: 1949 },
+];
+// Exibindo o título do segundo livro
+console.log("O segundo livro é:", biblioteca[1].titulo);
